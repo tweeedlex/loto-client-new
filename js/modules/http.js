19a0ccc7d37ff6bd539e036a980b798614cf1591
krutiy <7545245@ukr.net>
2023-10-08 20:09:37 +0300

    a

--- js/modules/http.js
+++ js/modules/http.js
@@ -1,6 +1,6 @@
 // export const API_URL = "https://app.24loto.com/api";
 // export const API_URL = "http://localhost:5001/api";
-export const API_URL = "http://loto-server-new.onrender.com/api";
+export const API_URL = "https://loto-server-new.onrender.com/api";
 
 const $api = axios.create({
   withCredentials: false,
